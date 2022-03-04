52965ec5e3c21ee2a64c2c56e40dbf0f31c6c5d6
Kaliatech <josh@jgstechnical.com>
2022-03-04 15:07:40 -0500

    Add basic follow behavior to message panel

--- src/js/3d/objects/MessagePanel.ts
+++ src/js/3d/objects/MessagePanel.ts
@@ -2,11 +2,30 @@ import { GuiPanel } from './GuiPanel'
 import * as GUI from '@babylonjs/gui/2D'
 import { LogicalScene } from '../../LogicalScene'
 import { white } from '../materials/ColorMaterials'
-import { Color3 } from '@babylonjs/core/Maths/math.color'
+import { FollowBehavior } from '@babylonjs/core'
+import { CameraChangedEvent } from '../../AppManagerEvents'
 
 export class MessagePanel extends GuiPanel {
+
+  private followBehavior: FollowBehavior
+  private cameraUnsub: { (): void }
+
   constructor(logicalScene: LogicalScene, name: string, protected msg: string) {
     super(logicalScene, name)
+
+    this.followBehavior = new FollowBehavior()
+    this.followBehavior.lerpTime = 250
+    this.followBehavior.ignoreCameraPitchAndRoll = true
+    this.followBehavior.pitchOffset = 0
+    this.followBehavior.defaultDistance = 2
+    this.followBehavior.maximumDistance = 4
+    this.followBehavior.orientToCameraDeadzoneDegrees = 30
+
+    // Not required because follow camera uses scene.activeCamera by default. Might be important if
+    // we need to support simultaneous XR and inline UIs though. ...though watching XR state is then probably better.
+    this.cameraUnsub = logicalScene.appManager.appBus.subscribe(CameraChangedEvent, (evt) => {
+      // this.followBehavior.followedCamera = evt.payload.camera
+    })
   }
 
   async init(): Promise<void> {
@@ -52,13 +71,20 @@ export class MessagePanel extends GuiPanel {
 
   load(): void {
     super.load()
+
+    this.followBehavior.attach(this.guiPanel);
+
+
   }
 
   unload(): void {
     super.unload()
+
+    this.followBehavior.detach()
   }
 
   dispose(): void {
+    this.cameraUnsub()
     this.dispose()
   }
 }
