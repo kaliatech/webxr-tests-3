7646bc5e3a95afda77f9d22c7dda96c26c2be018
Kaliatech <josh@jgstechnical.com>
2022-03-05 15:51:57 -0500

    Add video/photo test (wip)

--- src/js/scenes/Scene004-PhotoAndVideos.ts
+++ src/js/scenes/Scene004-PhotoAndVideos.ts
@@ -2,6 +2,7 @@ import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight'
 import { MeshBuilder } from '@babylonjs/core/Meshes/meshBuilder'
 import { Vector3 } from '@babylonjs/core/Maths/math.vector'
 
+import { PhotoDome } from '@babylonjs/core/Helpers/photoDome'
 import { VideoDome } from '@babylonjs/core/Helpers/videoDome'
 
 // This adds ~500k (100k gzipped) to build
@@ -17,9 +18,11 @@ import { AxesWidget } from '../3d/objects/AxesWidget'
 import { AppManager } from '../AppManager'
 import { WebXRAbstractMotionController } from '@babylonjs/core/XR/motionController/webXRAbstractMotionController'
 
+
 export class Scene004PhotoAndVideos extends LogicalScene {
   private leftMenu?: ControllerTrackingMenu
   private videoDome?: VideoDome
+  private photoDome?: PhotoDome
 
   constructor(appManager: AppManager) {
     super(appManager, false)
@@ -46,13 +49,33 @@ export class Scene004PhotoAndVideos extends LogicalScene {
     this.sceneAssetContainer.meshes.push(...axesWidget.root.getChildMeshes(false))
 
     this.createVideoDome()
+    //this.createPhotoDome()
+
     this.sceneAssetContainer.removeAllFromScene()
   }
 
+  private createPhotoDome() {
+    const photoDomeOpts = {
+      resolution: 32,
+      clickToPlay: true,
+      autoPlay: false,
+    }
+
+    let url = ""
+
+    //url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-002-photo/webxr-test-3-002-photo-048-insta360app-6080x3040-offset180-ps.jpg"
+    //url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-002-photo/webxr-test-3-002-photo-042-insta360app-5760x2880.jpg"
+    url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-002-photo/webxr-test-3-002-photo-041-insta360app-5760x2880.jpg"
+
+    this.photoDome = new PhotoDome('photoDome', url, photoDomeOpts, this.scene)
+    this.photoDome.fovMultiplier = 2.5
+
+
+    this.sceneAssetContainer.transformNodes.push(this.photoDome)
+    this.sceneAssetContainer.meshes.push(...this.photoDome.getChildMeshes()) // TODO: required?
+  }
+
   private createVideoDome() {
-    //const url = "https://yoda.blob.core.windows.net/videos/uptale360.mp4"
-    //const url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/uptale360.mp4"
-    //const url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/hughhou/Insta360Titan-7680_60fps_H265_100Mbps_360.mp4"
 
     const videoDomeOpts = {
       resolution: 32,
@@ -60,12 +83,47 @@ export class Scene004PhotoAndVideos extends LogicalScene {
       autoPlay: false,
     }
 
-    const url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/hughhou/output_zcam_180_3d.mp4'
+    // 180 Video Tests
+    // const url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/hughhou/output_zcam_180_3d.mp4'
+    //const url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/hughhou/output_zcam_180_3d.mp4'
+    // this.videoDome = new VideoDome('videoDome', url, videoDomeOpts, this.scene)
+    // this.videoDome.videoMode = VideoDome.MODE_SIDEBYSIDE
+    // this.videoDome.halfDome = true
+
+
+    // 360 Video Tests
+    //const url = "https://yoda.blob.core.windows.net/videos/uptale360.mp4"
+    //const url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/uptale360.mp4"
+    //const url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/hughhou/Insta360Titan-7680_60fps_H265_100Mbps_360.mp4"
+    let url = ""
+
+    //works on quest 2, not desktop
+    //url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-insta360-5760x2880-100Mbps-h265.mp4'
+    //url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-premiere-5760x2880-100Mbps-h265.mp4'
+    //url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-premiere-4096x2048-100Mbps-h265.mp4'
+
+    //works on both
+    url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-premiere-4096x2048-60Mbps-h264.mp4'
+    url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-insta360-5760x2880-100Mbps-h264.mp4'
+    url = 'https://temp.kaliatech.com/2022/webxr-tests-3/videos/kaliatech/webxr-tests-3-001-video/webxr-tests-3-video001-export-1-premiere-5760x2880-100Mbps-h264.mp4'
+
+
     this.videoDome = new VideoDome('videoDome', url, videoDomeOpts, this.scene)
-    this.videoDome.videoMode = VideoDome.MODE_SIDEBYSIDE
-    this.videoDome.halfDome = true
+    this.videoDome.videoMode = VideoDome.MODE_MONOSCOPIC
+    this.videoDome.halfDome = false
+    //this.videoDome.fovMultiplier = 0.8
+
+
+
+
+
+
+
+
+
+
     this.sceneAssetContainer.transformNodes.push(this.videoDome)
-    this.sceneAssetContainer.meshes.push(...this.videoDome.getChildMeshes())
+    this.sceneAssetContainer.meshes.push(...this.videoDome.getChildMeshes()) // TODO: required?
 
     //const url = "https://temp.kaliatech.com/2022/webxr-tests-3/videos/kandaovr/Dance_6k6k30_3dv.mp4"
     // const videoDome = new VideoDome("videoDome", url, videoDomeOpts, this.scene);
@@ -104,6 +162,7 @@ export class Scene004PhotoAndVideos extends LogicalScene {
 
   dispose() {
     this.leftMenu?.dispose()
+    this.photoDome?.dispose()
     this.videoDome?.dispose()
     super.dispose()
   }
--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -22,7 +22,12 @@
                     https://brianzinn.github.io/react-babylonjs/?path=/story/gui--with-2-dui
                   </a>
                 </li>
-
+                <li>
+                  <a href="https://www.youtube.com/watch?v=Q9vVYJd4bOE">
+                    Canon RF5.2mm F2.8 L DUAL FISHEYE Real World Test
+                  </a>
+                  (<a href="https://www.oculus.com/experiences/media/333140902004065/">Oculus TV</a>)
+                </li>
                 <li>
                   <a href="https://makepad.dev/">https://makepad.dev/</a>
                 </li>
